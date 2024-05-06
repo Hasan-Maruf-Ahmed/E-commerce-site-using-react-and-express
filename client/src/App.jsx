028313eb93100e8138151cb65ce8a08444943001
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-05-07 00:59:50 +0600

    Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,38 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 //* Pages
-import { Home } from './pages/Home'
-import { Category } from './pages/Category'
-import { Brands } from './pages/Brands'
-import { Trending } from './pages/Trending'
-import { Product } from './pages/Product'
-import { Login } from './pages/Login'
+import { Home } from "./pages/Home";
+import { Category } from "./pages/Category";
+import { Brands } from "./pages/Brands";
+import { Trending } from "./pages/Trending";
+import { Product } from "./pages/Product";
+import { Login } from "./pages/Login";
 
 //* Layouts
-import { RootLayout } from './layouts/RootLayout'
-
-
+import { RootLayout } from "./layouts/RootLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-        <Route index element={<Home/>} />
-        <Route path='category' element={<Category />} />
-        <Route path='brands' element={<Brands />} />
-        <Route path='trending' element={<Trending />} />
-        <Route path='product' element={<Product />} />
-        <Route path='login' element={<Login />} />
-    </Route>
+    <>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="category" element={<Category />} />
+        <Route path="brands" element={<Brands />} />
+        <Route path="trending" element={<Trending />} />
+        <Route path="product" element={<Product />} />
+      </Route>
+      <Route path="login" element={<Login />} />
+    </>
   )
-)
+);
 
 function App() {
-
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,24 @@
-
+import { Form } from "react-router-dom"
+import shopping from "../assets/shopping-animate.svg"
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className="fixed inset-0 flex justify-between items-center">
+      <div>
+        <Form>
+          <label>
+            <span>Email:</span>
+            <input type="email" name="email"></input>
+          </label>
+          <label>
+            <span>Password:</span>
+            <input type="password" name="password"></input>
+          </label>
+        </Form>
+      </div>
+      <div className="w-1/2">
+        <img src={shopping} className="w-full"/>
+      </div>
+    </div>
   )
 }
