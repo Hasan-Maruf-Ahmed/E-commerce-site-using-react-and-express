3d7020f80b23fcf37a759ac457a6f1916ffb2fd6
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-06-13 02:07:35 +0600

    Basic routing for admin panel.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,28 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+
+
+//* Pages
+import { AddProducts } from "./pages/AddProducts";
+import { ProductList } from "./pages/ProductList";
+
+//* Layouts
+import { AdminLayout } from "./layouts/AdminLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<AdminLayout />}>
+        <Route path="addproducts" element={<AddProducts/>} />
+        <Route path="productlist" element={<ProductList/>} />
+      </Route>
+    </>
+  )
+);
 
 function App() {
 
   return (
-    <>
-      <p className="text-3xl underline">
-        Admin
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
