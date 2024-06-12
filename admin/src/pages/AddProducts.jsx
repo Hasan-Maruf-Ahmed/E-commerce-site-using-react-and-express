a92d3ef37f135e742e14052de6eb641b48af8936
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-06-13 05:04:46 +0600

    Product add page basic

--- admin/src/pages/AddProducts.jsx
+++ admin/src/pages/AddProducts.jsx
@@ -1,7 +1,46 @@
-
+import { Form } from "react-router-dom";
 
 export const AddProducts = () => {
   return (
-    <div className="bg-yellow-300">AddProducts</div>
-  )
-}
+    <div className="m-12">
+      <Form className="space-y-4" method="post">
+        <label className="flex flex-col gap-4">
+          <span className="text-lg font-medium">Product Name:</span>
+          <input
+            className="max-w-80 w-full border-2 border-gray-400 rounded-md py-3 px-4 bg-transparent focus:border-orange-500 focus:outline-none"
+            type="text"
+            name="Name"
+            placeholder="Type here..."
+          />
+        </label>
+        <label className="flex flex-col gap-4">
+          <span className="text-lg font-medium">Product Name:</span>
+          <input
+            className="max-w-80 w-full border-2 border-gray-400 rounded-md py-3 px-4 bg-transparent focus:border-orange-500 focus:outline-none"
+            type="text"
+            name="Name"
+            placeholder="Type here..."
+          />
+        </label>
+        <label className="flex flex-col gap-4">
+          <span className="text-lg font-medium">Product Name:</span>
+          <input
+            className="max-w-80 w-full border-2 border-gray-400 rounded-md py-3 px-4 bg-transparent focus:border-orange-500 focus:outline-none"
+            type="text"
+            name="Name"
+            placeholder="Type here..."
+          />
+        </label>
+        <label className="flex flex-col gap-4">
+          <span className="text-lg font-medium">Product Name:</span>
+          <input
+            className="max-w-80 w-full border-2 border-gray-400 rounded-md py-3 px-4 bg-transparent focus:border-orange-500 focus:outline-none"
+            type="text"
+            name="Name"
+            placeholder="Type here..."
+          />
+        </label>
+      </Form>
+    </div>
+  );
+};
