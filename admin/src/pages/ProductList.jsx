8dd7eee1be80077d01cb65377ceb8faf3682e1de
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-06-23 09:09:37 +0600

    Product List page table structure.

--- admin/src/pages/ProductList.jsx
+++ admin/src/pages/ProductList.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
 
 
 export const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  
   return (
-    <div>ProductList</div>
+    <div>
+      <h4>Products List</h4>
+      <div>
+        <table className="w-full mx-auto">
+          <thead>
+            <tr>
+              <th className="p-2">Products</th>
+              <th className="p-2">Name</th>
+              <th className="p-2">Category</th>
+              <th className="p-2">Price</th>
+              <th className="p-2">Stock</th>
+              <th className="p-2">Rating</th>
+              <th className="p-2">Remove</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
   )
 }
