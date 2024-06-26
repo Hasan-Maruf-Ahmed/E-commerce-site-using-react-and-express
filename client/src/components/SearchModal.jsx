04104d287a5af6d2c695141c01cdc1924fb826e1
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-06-26 11:08:05 +0600

    Optimized the searchbar to clear search result after closing the searchbar.

--- client/src/components/SearchModal.jsx
+++ client/src/components/SearchModal.jsx
@@ -12,6 +12,7 @@ export const SearchModal = ({ open, onClose }) => {
     const handleClickOutside = (e) => {
       if (modalRef.current && !modalRef.current.contains(e.target)) {
         onClose();
+        setResults([]);
       }
     };
 
@@ -25,13 +26,14 @@ export const SearchModal = ({ open, onClose }) => {
     return null;
   }
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div ref={modalRef} >
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-sm">
+      <div ref={modalRef} className="min-w-[600px]">
         <SearchBar setResults={setResults}/>
         <div className="w-full py-4 px-6 bg-white flex flex-col shadow-xl rounded-lg mt-4 max-h-48 overflow-auto">
             {results.length !== 0 &&
             (results.map((result, id) => {
-                return <div key={id} className="mb-4"><Link to={`/product/${result._id}`} onClick={() => onClose(false)}>{result.name}</Link></div>
+                return <div key={id} className="mb-4"><Link className="truncate" to={`/product/${result._id}`} onClick={() => {onClose()
+              setResults([])}}>{result.name}</Link></div>
             }))
             }
         </div>
